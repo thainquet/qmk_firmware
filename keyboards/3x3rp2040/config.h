3ebe1fb7c744b50357d8827f292656aa6754c06a
thainquet <thainq00@gmail.com>
2025-09-14 20:04:05 +0700

    feat: update oled usage

--- keyboards/3x3rp2040/config.h
+++ keyboards/3x3rp2040/config.h
@@ -4,9 +4,12 @@
 #define I2C_DRIVER I2CD0
 #define I2C1_SDA_PIN GP0
 #define I2C1_SCL_PIN GP1
-// #define OLED_DISPLAY_128X64
-// #define OLED_DISPLAY_WIDTH 128
-// #define OLED_DISPLAY_HEIGHT 64
+#define OLED_DISPLAY_128X64
+#define OLED_DISPLAY_WIDTH 128
+#define OLED_DISPLAY_HEIGHT 64
+#define OLED_IC OLED_IC_SH1106
+#define OLED_COLUMN_OFFSET 2
+#define OLED_TIMEOUT 0
 
 #include_next <mcuconf.h> // for i2c
 #undef RP_I2C_USE_I2C0
